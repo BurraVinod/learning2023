f73c86ca93fc89c651ec6007ff0f6d7d9c680a58
BurraVinod <135966594+BurraVinod@users.noreply.github.com>
2023-06-18 15:24:15 +0530

    Add files via upload

---
+++ oddandevenindexes.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+int n;
+scanf("%d",&n);
+int a[n],sum=0,su=0;
+for(int i=0;i<n;i++){
+scanf("%d",&a[i]);
+}
+for(int i=0;i<n;i++){
+if(i%2==0){
+sum=sum+a[i];
+
+
+}
+else
+{
+su=su+a[i];
+
+}
+}
+printf("even %d\n",sum);
+printf("odd %d\n",su);
+
+
+}
